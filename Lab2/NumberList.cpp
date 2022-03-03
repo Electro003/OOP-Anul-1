ada6dd79dc39bff9f185748816e8338737542b0e
DESKTOP-3MVKK1O\Teodor <iteodordimi@gmail.com>
2022-03-03 09:25:15 +0200

    Sort

--- Lab2/NumberList.cpp
+++ Lab2/NumberList.cpp
@@ -10,3 +10,15 @@ bool NumberList::Add(int x)
 	this->numbers[this->count] = x;
 	this->count++;
 }
+
+void NumberList::Sort()
+{
+	for (int i = 0; i < count - 1; i++)
+		for (int j = i + 1; j < count; j++)
+			if (numbers[i] > numbers[j])
+			{
+				numbers[i] += numbers[j];
+				numbers[j] = numbers[i] - numbers[j];
+				numbers[i] -= numbers[j];
+			}
+}
